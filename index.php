427d01330b5bf5427dd3270d2b4dccbd7001e9f7
Tom Kren <tomkren@gmail.com>
2013-11-08 15:57:43 +0100

    vylepšení....

--- index.php
+++ index.php
@@ -20,10 +20,19 @@ header('Content-Type: application/javascript; charset=utf-8');
 //TODO : předělat na bezpečnější skrz nějaký to PDO::prepare nebo jak to je
 // např bug: key #zobrazeni dela divnost
 
+$handlerID = array_key_exists('id',$_GET) ? $_GET['id'] : '';
+
+
+function esc ($str) {
+  return str_replace ( "\n" , "\\n" , $str );
+}
+
 function ret ($str) {
-  echo "FishtronDB.handle(". $str .");";  
+  global $handlerID;
+  echo "FishtronDB.handle('".$handlerID."', ". esc($str) .");";  
   exit;
 }
+
 
 switch($_SERVER['REQUEST_METHOD']){
 
@@ -35,9 +44,9 @@ switch($_SERVER['REQUEST_METHOD']){
 
       case 'get':
 
-        $key = $_GET['key'];
+        $key = $dbh->quote($_GET['key']);
 
-        $q = "SELECT `val` FROM `big_object` WHERE `key` = '$key';";
+        $q = "SELECT `val` FROM `big_object` WHERE `key` = $key";
 
         $result = $dbh->query($q);
         $o = $result->fetchObject();
@@ -52,16 +61,16 @@ switch($_SERVER['REQUEST_METHOD']){
 
       case 'set' :
 
-        $key = $_GET['key'];
-        $val = $_GET['val'];
+        $key = $dbh->quote($_GET['key']);
+        $val = $dbh->quote($_GET['val']);
 
-        $q = "UPDATE `big_object` SET `val` = '$val' WHERE `key` =  '$key';";
+        $q = "UPDATE `big_object` SET `val` = $val WHERE `key` =  $key";
 
         $result = $dbh->query($q);
 
         // TODO : nerozezná to když tam takovej neni a když tam je ale vkladana hodnota je stejná
         if ($result->rowCount() === 0) {
-          $q = "INSERT INTO `big_object` (`key`, `val`) VALUES ('$key', '$val');";
+          $q = "INSERT INTO `big_object` (`key`, `val`) VALUES ($key, $val)";
           $dbh->query($q);
         }
 
@@ -74,12 +83,13 @@ switch($_SERVER['REQUEST_METHOD']){
         $q = "SELECT * FROM `big_object` WHERE 1;"; 
         $result = $dbh->query($q);
 
-        echo "FishtronDB.handle({";
+        echo "FishtronDB.handle('".$handlerID."', {\n";
         while ($o = $result->fetchObject()) {
+          echo "  ";
           echo $o->key;
-          echo ":";
-          echo $o->val;
-          echo ',';
+          echo " : ";
+          echo esc($o->val);
+          echo ",\n";
         }
         echo "});"; exit;
 
